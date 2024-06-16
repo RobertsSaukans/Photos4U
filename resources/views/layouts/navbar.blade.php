323089248f848656286ef445bb6b51e786597f98
RobertsSaukans <roberts.saukans@.com>
2024-06-16 21:26:08 +0300

    updated navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,3 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Photos4U') }}</title>
+
+    <!-- Bootstrap CSS !-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet"> 
+    
+    <!-- Scripts 
+   @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+</head>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="{{ url('/') }}">Photos4U</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
